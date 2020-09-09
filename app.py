594eacedb6f744c946fbe2f72c864932038d5511
Krishaay Jois <62088268+krishaayjois21@users.noreply.github.com>
2020-09-09 23:07:10 +0530

    Heroku Deployment Fix

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
-from main import app
-app.run(debug=True)
+from flask import Flask
+from flask_restful import Api , Resource
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Restrictions(Resource):
+    def get(self):
+        cargo_limit = 340000
+        collateral_limit = 15000000000
+        return {
+            "message": "success",
+            "data": 
+            {
+                "volume_limit": cargo_limit,
+                "collateral_limit": collateral_limit
+            }
+        }, 200
+
+api.add_resource(Restrictions , "/api/restrictions")
+
+if __name__="__main__":
+    app.run(debug=True)
